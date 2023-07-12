c6c5c6a8b9e48288a94ec50c013e879e87b4bb6f
AlanS2405 <alan.dario.solis@gmail.com>
2023-07-12 12:09:54 -0300

    cree el componente filtro por nombre y lo inserté en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllCountriesThunk } from "./store/slices/allCountries.slice.js";
+import FilterByName from "./components/FilterByName";
 
 function App() {
+  const [inputFilterName, setInputFilterName] = useState("");
+
+  const countries = useSelector((states) => states.allCountries);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCountriesThunk());
   }, []);
 
+  const filterCountriesByName = countries?.filter((country) => {
+    const countryName = country.name.common.toLowerCase();
+    const currentInput = inputFilterName.toLowerCase();
+    return countryName.includes(currentInput);
+  });
+
+  console.log(countries);
+
   return (
     <>
       <div>
         <h1>App</h1>
+        <FilterByName
+          inputFilterName={inputFilterName}
+          setInputFilterName={setInputFilterName}
+        />
+        <ul>
+          {filterCountriesByName?.map((country) => (
+            <li key={country.name.common}>{country.name.common}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
