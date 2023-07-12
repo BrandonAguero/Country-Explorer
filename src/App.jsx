b7c18ee33ba18ae39b02b9c90e0e8ed9178e3a1f
Alejandro velosa <alejovelosa4@gmail.com>
2023-07-12 14:23:40 -0500

    se llama un estado apra realizar el selectcontinent  se agrega la function del select  y importa elcomponente filterByCountinent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { getAllCountriesThunk } from "./store/slices/allCountries.slice.js";
+import FilterByName from "./components/FilterByName";
+import FilterByContinent from "./components/FilterByContinent";
 
 function App() {
+  const [selectedContinent, setSelectedContinent] = useState("");
+
+  const [inputFilterName, setInputFilterName] = useState("");
+
+  const countries = useSelector((states) => states.allCountries);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllCountriesThunk());
+  }, []);
+
+  const filterCountriesByName = countries?.filter((country) => {
+    const countryName = country.name.common.toLowerCase();
+    const currentInput = inputFilterName.toLowerCase();
+    const continent = country.region.toLowerCase();
+    return countryName.includes(currentInput) &&
+      (selectedContinent === '' || continent === selectedContinent.toLowerCase())
+  });
+
+  console.log(countries);
+
   return (
     <>
       <div>
         <h1>App</h1>
+        <FilterByName
+          inputFilterName={inputFilterName}
+          setInputFilterName={setInputFilterName}
+        />
+        <FilterByContinent
+          selectedContinent={selectedContinent}
+          setSelectedContinent={setSelectedContinent}
+        />
+        <ul>
+          {filterCountriesByName?.map((country) => (
+            <li key={country.name.common}>{country.name.common}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
